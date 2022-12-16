f33590e8945802ab057618e55c04c2f6a3ef38f7
motherboard7444 <61032704+motherboard7444@users.noreply.github.com>
2022-12-16 14:26:30 -0500

    Update LiftOpMode.java

--- LiftOpMode.java
+++ LiftOpMode.java
@@ -1,253 +1,105 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
-/**
- * Original created by ashley.peake on 8/30/2018.
- * modified by KPSorrells (for teaching RoboLearners) 11/26/2022
- */
+@TeleOp
+public class LiftOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // position of the dr4b when it is on the **ground
+        int liftGroundPosition = 50;
 
-@Disabled
-public class HardwarePowerPlay {
+        // position of dr4b when it is **low
+        int liftLowPosition = 250;
 
-    //Wheels
-    public DcMotor motorFrontLeft;
-    public DcMotor motorBackLeft;
-    public DcMotor motorFrontRight;
-    public DcMotor motorBackRight;
-    public DcMotor leftLift;
-    public DcMotor rightLift;
+        // position of dr4b when it is **mid
+        int liftMidPosition = 450;
 
+        // position of dr4b when it is **high
+        int liftHighPosition = 650;
 
-    //DoubleReverse 4-Bar Lift
-    //public DcMotor barLeft;
-    //public DcMotor barRight;
-    //public DcMotor leftSlide;
-    //public DcMotor rightSlide;
+        // find a motor in the hardware map named these
+        DcMotor rightLift = hardwareMap.dcMotor.get("rightLift");
+        DcMotor leftLift = hardwareMap.dcMotor.get("leftLift");
 
-    //Servos or Claw - CR = continuos Servo
-    //public CRServo sweeperLeft;
-    //public CRServo sweeperRight;
-    //public Servo boxLid;
-    //public CRServo duckSpin;
+        rightLift.setDirection(DcMotor.Direction.REVERSE);
+        leftLift.setDirection(DcMotor.Direction.FORWARD);
 
-    //Sets variable driveTime as an integer
-    int driveTime;
+        // reset the motor encoder so that it reads zero ticks
+        rightLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        //Added to tell motor to use encoder position when buttons pressed
+        leftLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-//----------------------------Initialize Robot ---------------------------------
-   /*  This method allows us to initialize the motors and sensors only once.
-       It is called and used in every other program after "Init" is pressed.
-    */
+        // sets the starting position of the arm to the down position
+        rightLift.setTargetPosition(liftGroundPosition);
+        rightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftLift.setTargetPosition(liftGroundPosition);
+        leftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-    public void InitializeRobot(HardwareMap hwMap) {
+        waitForStart();
 
-        HardwareMap HWMap = hwMap;
+        while (opModeIsActive()) {
+            // if the x button is pressed, move to **ground
+            if (gamepad1.x) {
+                rightLift.setPower(0.75);
+                leftLift.setPower(0.75);
+                rightLift.setTargetPosition(liftGroundPosition);
+                rightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                leftLift.setTargetPosition(liftGroundPosition);
+                leftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
 
-        //initialize Wheels
-        motorFrontLeft= HWMap.dcMotor.get("motorFrontLeft");
-        motorBackLeft= HWMap.dcMotor.get("motorBackLeft");
-        motorFrontRight= HWMap.dcMotor.get("motorFrontRight");
-        motorBackRight= HWMap.dcMotor.get("motorBackRight");
+            // if the y button is pressed, move to **high
+            if (gamepad1.y) {
+                rightLift.setPower(0.75);
+                leftLift.setPower(0.75);
+                rightLift.setTargetPosition(liftHighPosition);
+                rightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                leftLift.setTargetPosition(liftHighPosition);
+                leftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
 
-        // Reverse the right side motors
-        // Reverse left motors if you are using NeveRests
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            // if the b button is pressed, move to **mid
+            if (gamepad1.b) {
+                rightLift.setPower(0.75);
+                leftLift.setPower(0.75);
+                rightLift.setTargetPosition(liftMidPosition);
+                rightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                leftLift.setTargetPosition(liftMidPosition);
+                leftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
 
-        //Initialize DoubleReverse 4-Bar Lift
-        leftLift= HWMap.dcMotor.get("leftLift");
-        rightLift= HWMap.dcMotor.get("rightLift");
-        // leftSlide = HWMap.dcMotor.get("leftSlide");
-        //rightSlide = HWMap.dcMotor.get("rightSlide");
+            // if the a button is pressed, move to **low
+            if (gamepad1.a) {
+                rightLift.setPower(0.75);
+                leftLift.setPower(0.75);
+                rightLift.setTargetPosition(liftLowPosition);
+                rightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                leftLift.setTargetPosition(liftLowPosition);
+                leftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
 
-        //Initialize Servos
-        //sweeperLeft = HWMap.crservo.get("sweeperLeft");
-        //sweeperRight = HWMap.crservo.get("sweeperRight");
-        //boxLid = HWMap.servo.get("boxLid");
-        //duckSpin = HWMap.crservo.get("duckSpin");
+            // get the current position of the motor
+            double position = rightLift.getCurrentPosition();
+            position = leftLift.getCurrentPosition();
 
-    }  //end of method InitializeRobot
+            // get the target position of the motor
+            double desiredPosition = rightLift.getTargetPosition();
+            desiredPosition = leftLift.getTargetPosition();
 
+            // show the position of the motor on telemetry
+            telemetry.addData("Encoder Position", position);
 
-//--------------------------Driving Pathways------------------------------------
+            // show the target position of the motor on telemetry
+            telemetry.addData("Desired Position", desiredPosition);
 
-   /*  This method allows the robot to drive forward based on encoder values.
-       A distance is given that is converted to an encoder position in the code.
-       leftDirection and rightDirection set the direction of the motors to allow
-       the robot to either move straight ot turn.
-    */
-
-    public void DriveStraight(double power, double totalSeconds, int Direction) throws InterruptedException {
-
-        //For driving forward or backward
-        // declare variables for this method (power, totalSeconds (milliseconds) & Direction)
-        //For forwards set direction = 1 (In method call)
-        // For backwards set direction = -1 (In method call)
-        //example: driveStraight(1, 5, 1) means drive straight at 100% power, for 5 seconds, in forward direction
-        //example: driveStraight(1, 5, -1) means drive straight at 100% power, for 5 seconds, in backwards direction
-
-        motorFrontLeft.setPower(power * Direction);
-        motorBackLeft.setPower(power * Direction);
-        motorFrontRight.setPower(power * Direction);
-        motorBackRight.setPower(power * Direction);
-
-        Thread.sleep((long) totalSeconds);
-
-        //Stop Robot
-        motorFrontLeft.setPower(0.0);
-        motorBackLeft.setPower(0.0);
-        motorFrontRight.setPower(0.0);
-        motorBackRight.setPower(0.0);
-
-    } //End DriveStraight Method
-
-    public void DriveSideways(double power, long totalSeconds, int Direction) throws InterruptedException {
-
-        //For strafing to the left or the right
-        // declare variables for this method (power, totalSeconds (milliseconds) & Direction)
-        //For right motion set direction = 1 (In method call)
-        //For left motion set direction = -1 (In method call)
-        //example: driveSideways(.5, 3, 1) means drive straight at 50% power, for 3 seconds, in right direction
-        //example: driveSideways(.75, 5, -1) means drive straight at 75% power, for 5 seconds, in left direction
-
-        motorFrontLeft.setPower(power * Direction);
-        motorBackLeft.setPower(power * -Direction);
-        motorFrontRight.setPower(power * -Direction);
-        motorBackRight.setPower(power * Direction);
-
-        Thread.sleep(totalSeconds);
-
-        // stops all motion
-        motorFrontLeft.setPower(0.0);
-        motorBackLeft.setPower(0.0);
-        motorFrontRight.setPower(0.0);
-        motorBackRight.setPower(0.0);
-
-    } //Ends DriveSideways Method
-
-    public void DiagonolForward(double power, long totalSeconds, int Direction) throws InterruptedException {
-
-        //For driving forward in a diagonal direction
-        // declare variables for this method (power, totalSeconds (milliseconds) & Direction)
-        //For right motion set direction = 1 (In method call)
-        //For left motion set direction = -1 (In method call)
-        //example: DiagonolForward(.8, 3, 1) means drive straight at 80% power, for 3 seconds, in forward right direction
-        //example: DiagonalForward(.75, 5, -1) means drive straight at 75% power, for 5 seconds, in forward left direction
-
-        if (Direction == 1) {
-
-            motorFrontLeft.setPower(power * Direction);
-            motorBackLeft.setPower(0);
-            motorFrontRight.setPower(0);
-            motorBackRight.setPower(power * Direction);
-
-            Thread.sleep(totalSeconds);
+            telemetry.update();
         }
-
-        if (Direction == -1) {
-
-            motorFrontLeft.setPower(0);
-            motorBackLeft.setPower(power * -Direction);
-            motorFrontRight.setPower(power * -Direction);
-            motorBackRight.setPower(0);
-
-            Thread.sleep(totalSeconds);
-        }
-
-        // stops all motion
-
-        motorFrontLeft.setPower(0.0);
-        motorBackLeft.setPower(0.0);
-        motorFrontRight.setPower(0.0);
-        motorBackRight.setPower(0.0);
-
-    } //End Diagonal Forward Method
-
-
-    public void DiagonolBackward(double power, long totalSeconds, int Direction) throws InterruptedException {
-
-        //For driving forward in a diagonal direction
-        // declare variables for this method (power, totalSeconds (milliseconds) & Direction)
-        //For right motion set direction = 1 (In method call)
-        //For left motion set direction = -1 (In method call)
-        //example: DiagonolBackward(.8, 3, 1) means drive straight at 80% power, for 3 seconds, in back Right direction
-        //example: DiagonalForward(.75, 5, -1) means drive straight at 75% power, for 5 seconds, in left direction
-
-        if (Direction == 1) {
-
-            motorFrontLeft.setPower(0);
-            motorBackLeft.setPower(power * -Direction);
-            motorFrontRight.setPower(power * -Direction);
-            motorBackRight.setPower(0);
-
-            Thread.sleep(totalSeconds);
-        }
-
-        if (Direction == -1) {
-
-            motorFrontLeft.setPower(power * Direction);
-            motorBackLeft.setPower(0);
-            motorFrontRight.setPower(0);
-            motorBackRight.setPower(power * Direction);
-
-            Thread.sleep(totalSeconds);
-        }
-
-        // stops all motion
-
-        motorFrontLeft.setPower(0.0);
-        motorBackLeft.setPower(0.0);
-        motorFrontRight.setPower(0.0);
-        motorBackRight.setPower(0.0);
-
-    } //End Diagonal Backward Method
-
-
-    public void CenterSpin(double power, long totalSeconds, int Direction) throws InterruptedException {
-
-        //For turning robot on center
-        // declare variables for this method (power, totalSeconds (milliseconds) & Direction)
-        //For right motion set direction = 1 (In method call)
-        //For left motion set direction = -1 (In method call)
-        //180 degree spin at 100% power takes -----; at 75% takes ----; at 50% takes
-        //90 degree spring at 100% power takes----; at 75% takes ----; at 50% takes
-        //Check BATTERY Power
-
-        //example: CenterSpin(1, 3, 1) means spin at 100% power, for 3 seconds, to the right
-
-        if (Direction == 1) {
-
-            motorFrontLeft.setPower(power * Direction);
-            motorBackLeft.setPower(power * Direction);
-            motorFrontRight.setPower(power * -Direction);
-            motorBackRight.setPower(power * -Direction);
-
-            Thread.sleep(totalSeconds);
-        }
-
-        if (Direction == -1) {
-
-            motorFrontLeft.setPower(power * Direction);
-            motorBackLeft.setPower(power * Direction);
-            motorFrontRight.setPower(power * -Direction);
-            motorBackRight.setPower(power * -Direction);
-
-            Thread.sleep(totalSeconds);
-        }
-
-        // stops all motion
-
-        motorFrontLeft.setPower(0.0);
-        motorBackLeft.setPower(0.0);
-        motorFrontRight.setPower(0.0);
-        motorBackRight.setPower(0.0);
-
-    } //End CenterSpin Method
-
+    }
 }
